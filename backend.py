c49cf542ab6f2e83285b039ffdd6b82870c95a45
Paskahlis Anjas Prabowo <13515108@std.stei.itb.ac.id>
2017-11-28 15:24:05 +0700

    fix mismatch columns, failed to load model

--- backend.py
+++ backend.py
@@ -38,15 +38,8 @@ def process():
                     ('hours_per_week', [hours_per_week]),
                     ('native_country', [native_country])]
 
-        
-
         import pandas as pd
         df = pd.DataFrame.from_items(features)
-
-        # Replacing '?' data with its prior
-        # for value in ['workclass', 'education', 'marital_status', 'occupation',
-        #         'relationship','race', 'sex', 'native_country', 'income']:
-        #     df[value].replace(['?'], [df.describe(include='all')[value][2]],inplace=True)
 
         # Labelling data
         from sklearn import preprocessing
@@ -54,12 +47,10 @@ def process():
 
         le = preprocessing.LabelEncoder()
 
-        # workclass_cat = le.fit_transform(df.workclass)
+        workclass_cat = le.fit_transform(df.workclass)
         marital_cat   = le.fit_transform(df.marital_status)
         occupation_cat = le.fit_transform(df.occupation)
         relationship_cat = le.fit_transform(df.relationship)
-        # income_cat = le.fit_transform(df.income)
-        # race_cat = le.fit_transform(df.race)
         native_country_cat = le.fit_transform(df.native_country)
         education_cat = le.fit_transform(df.education)
 
@@ -76,22 +67,13 @@ def process():
         del df['age']
         del df['education']
         del df['fnlwgt']
-        del df['hours_per_week']
 
-        print(df)
-
-        # # perform one hot encoder for some invalueable attributes
-        # ['gender', 'race', 'workclass']
-        
-        df.loc[1] = ['?', 0, 0, 0, 0, 'White', 'Male', 0, 0, 0]
-        df.loc[2] = ['Self-emp-not-inc', 0, 0, 0, 0, 'Asian-Pac-Islander', 'Female', 0, 0, 0]
-        df.loc[3] = ['Self-emp-inc', 0, 0, 0, 0, 'Asian-Pac-Islander', 'Female', 0, 0, 0]
-        df.loc[4] = ['Federal-gov', 0, 0, 0, 0, 'Amer-Indian-Eskimo', 'Female', 0, 0, 0]
-        df.loc[5] = ['Local-gov', 0, 0, 0, 0, 'Black', 'Female', 0, 0, 0]
-        df.loc[6] = ['State-gov', 0, 0, 0, 0, 'Other', 'Female', 0, 0, 0]
-        df.loc[7] = ['Without-pay', 0, 0, 0, 0, 'Other', 'Female', 0, 0, 0]
-        df.loc[8] = ['Never-worked', 0, 0, 0, 0, 'Other', 'Female', 0, 0, 0]
-        df.loc[8] = ['Private', 0, 0, 0, 0, 'Other', 'Female', 0, 0, 0]
+        df.loc[1] = [0, 0, 0, 0, 0, 'White', 'Male', 0, 0, 0, 0]
+        df.loc[2] = [0, 0, 0, 0, 0, 'Asian-Pac-Islander', 'Female', 0, 0, 0, 0]
+        df.loc[3] = [0, 0, 0, 0, 0, 'Asian-Pac-Islander', 'Female', 0, 0, 0, 0]
+        df.loc[4] = [0, 0, 0, 0, 0, 'Amer-Indian-Eskimo', 'Female', 0, 0, 0, 0]
+        df.loc[5] = [0, 0, 0, 0, 0, 'Black', 'Female', 0, 0, 0, 0]
+        df.loc[6] = [0, 0, 0, 0, 0, 'Other', 'Female', 0, 0, 0, 0]
 
         encoded = pd.get_dummies(df, prefix=['workclass', 'race', 'sex'])
         print(encoded)
